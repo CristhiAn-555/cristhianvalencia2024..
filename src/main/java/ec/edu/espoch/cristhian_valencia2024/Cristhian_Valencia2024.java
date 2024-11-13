9863062aaceac7187b05e14f881c72f79b4fd3ff
CristhiAn-555 <cristhianvalencia999@gmail.com>
2024-11-13 14:51:10 -0500

    hola

---
+++ src/main/java/ec/edu/espoch/cristhian_valencia2024/Cristhian_Valencia2024.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoch.cristhian_valencia2024;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author PERSONAL
+ */
+public class Cristhian_Valencia2024 {
+
+    public static void main(String[] args) {
+       
+        Scanner sc = new Scanner(System.in);
+        int[] dias = new int[7];
+        double  promedio = 0, puntuacionAlto = 0, puntuacionBajo = 0;
+        int productosEncimaPromedio = 0;
+        
+        
+        //p2
+        
+        System.out.println("Ingresar la putuacion diarias");
+        for(int i = 0; i < dias.length; i++){
+            
+            System.out.print("puntuacion diarias " + (i + 1) + ": ");
+           dias[i] = scanner.nextDouble();
+        }
+        
+        int opcion;
+        do {
+            
+            System.out.println("Mostrar los 5 menu");
+            System.out.println("1.calcular el total de puntuaciones de la semana");
+            System.out.println("2.calcular el promedio de puntuaciones y mostrar los dias por encima y por debajo del promedio");
+            System.out.println("3.mostrar el dia con la puntuciones mas alta y el dia con la puntuacion mas baja");
+            System.out.println("4.calcular el total de puntuciones acumuladas durante los primeros cuatro dias y los ultimos cuatro dias");
+            System.out.println("5.salir");
+            System.out.print("Seleccione una opción: ");
+            opcion = scanner.nextInt();
+        
+            
+    
+    }
+    
+}
